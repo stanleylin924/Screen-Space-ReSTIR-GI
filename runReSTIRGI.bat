56ce6bb3152885f878f234d752bbd77721e569c9
stanleylin924 <stanleylin924@gmail.com>
2024-03-16 20:37:05 +0800

    Add running command.

---
+++ runReSTIRGI.bat
@@ -0,0 +1,2 @@
+Bin\x64\Release\Mogwai.exe --script=Scripts\ReSTIRGIPassWithGBuffer.py --scene=..\Media\Arcade\Arcade.pyscene
+REM Bin\x64\Release\Mogwai.exe --script=Scripts\ReSTIRGIPassWithGBuffer.py --scene=..\Data\VeachAjar\VeachAjar.pyscene
